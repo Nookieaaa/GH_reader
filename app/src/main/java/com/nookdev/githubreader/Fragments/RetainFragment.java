ea8d3cc74e14ad28c321e62455be29866c373beb
Anton <nookieaaa@hotmail.com>
2015-07-17 00:12:32 +0300

    16.07.2015

--- app/src/main/java/com/nookdev/githubreader/Fragments/RetainFragment.java
+++ app/src/main/java/com/nookdev/githubreader/Fragments/RetainFragment.java
@@ -102,8 +102,8 @@ public class RetainFragment extends Fragment{
                 //TODO добавить авторизацию и убрать свой логин
 
 
-                //GitHub gitHub = GitHub.connectAnonymously();
-                GitHub gitHub = GitHub.connectUsingPassword("nookieaaa","nookie1");
+                GitHub gitHub = GitHub.connectAnonymously();
+
 
 
 
